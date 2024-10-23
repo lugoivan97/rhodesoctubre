f00f5b1235fe4f0c34a04f359c8e14dccacc2a60
Ivan Lugo <ivanlugo97@gmail.com>
2024-10-23 01:30:12 -0300

    Primera subida

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
